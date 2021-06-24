911a2446b3ab52d4fa0296b6d3fe229945bef50f
Adrian Kennard <a@k.gg>
2021-06-24 08:05:38 +0100

    Logout

--- dologout.c
+++ dologout.c
@@ -14,19 +14,28 @@
 
 const char *dologout(SQL * sqlp, const char *session)
 {
-   const char *v;
    if (!sqlp)
       return "No sql";
+   if (!session || !*session)
+      return "No session";
    if (!session || !*session)
       return "No session";
 #ifdef CONFIG_DB_DATABASE
    if (*CONFIG_DB_DATABASE)
       sql_safe_select_db(sqlp, CONFIG_DB_DATABASE);
 #else
+   const char *v;
    if (*CONFIG_ENV_DB && !(v = getenv(CONFIG_ENV_DB)) || !*v)
       return "No database";
    sql_safe_select_db(sqlp, v);
 #endif
+#ifdef	DB_SEPARATE_SESSION
+   sql_safe_query_free(sqlp, sql_printf("DELETE FROM `%#S` WHERE `%#S`=%#s", CONFIG_DB_SESSION_TABLE, CONFIG_DB_SESSION_FIELD, session));
+#else
+   sql_safe_query_free(sqlp, sql_printf("UPDATE `%#S` SET `%#S`=NULL WHERE `%#S`=%#s", CONFIG_DB_USER_TABLE, CONFIG_DB_SESSION_FIELD, CONFIG_DB_SESSION_FIELD, session));
+#endif
+   if (!sql_affected_rows(sqlp))
+      return "Was not logged in";
    return NULL;
 }
 
